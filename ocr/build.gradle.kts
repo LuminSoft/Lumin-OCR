617c97f11f9ab5432f80bb0d75d5a31448660399
luminsoft <andrew_samir93@hotmail.com>
2024-11-06 13:13:39 +0200

    face detection

--- ocr/build.gradle.kts
+++ ocr/build.gradle.kts
@@ -37,6 +37,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    // Face Detection
+    implementation ("com.google.mlkit:face-detection:16.1.5")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
